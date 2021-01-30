15a4b5712924e6c8796ac8833abd0bc2d3bf67df
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-30 13:30:16 -0500

    Typo

--- src/EarningsAlleyEngine.cs
+++ src/EarningsAlleyEngine.cs
@@ -19,7 +19,7 @@ namespace EarningsAlley
     {
         //ENVIRONMENT VARIABLES HERE
         private static string RecentlyCompletedTranscriptUrlsBlockBlobName = "RecentlyCompletedTranscriptUrls";
-        private static string RecentlyObservedForm4FilingsFileName = "RecentlyBbservedForm4Filings";
+        private static string RecentlyObservedForm4FilingsFileName = "RecentlyObservedForm4Filings";
         
         //Private resources here
         private EarningsAlleyLoginPackage LoginPackage;
